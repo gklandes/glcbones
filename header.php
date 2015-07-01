d5c69557f0941ad49ba3c3502011d3958bc23a29
gabelandes <gabe.landes@ddifrontline.com>
2015-06-30 20:47:55 -0500

    remove container class from header nav element

--- header.php
+++ header.php
@@ -49,7 +49,6 @@
                 <div id="navbar" class="navbar-collapse collapse">
                     <?php wp_nav_menu(array(
                         'container' => false, // remove nav container
-                        'container_class' => 'menu cf', // class of container (should you choose to use it)
                         'menu' => __( 'The Main Menu', 'bonestheme' ), // nav name
                         'menu_class' => 'nav navbar-nav', // adding custom nav class
                         'theme_location' => 'main-nav', // where it's located in the theme
