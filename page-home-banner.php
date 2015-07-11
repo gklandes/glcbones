35db897110f23cd8b8c752fbccc1bd026bfbb57c
gabelandes <gabe.landes@ddifrontline.com>
2015-07-11 07:21:10 -0500

    rename jumbotron home and update banner image style

---
+++ page-home-banner.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ Template Name: Banner Home
+*/
+?>
+<?php get_header(); ?>
+<main id="main" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <?php
+        if (has_post_thumbnail( $post->ID )) {
+            $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+            $jtBack = 'url('.$image[0].')';
+        } else {
+            $jtBack = 'none';
+        }
+    ?>
+    <div class="jumbotron" style="background-image: <?php echo $jtBack; ?>;">
+        <div class="container">
+            <?php the_content(); ?>
+        </div>
+    </div>
+    <?php endwhile; endif; ?>
+    <!-- Landing Page Cards -->
+    <div class="container">
+        <div class="row">
+            <?php $query = new WP_Query( array( 
+                'post_type' => 'page',
+                // 'posts_per_page' => 3
+                'posts_per_page' => 3,
+                'tag' => 'landing'
+            ) );
+            if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+            <div class="col-md-4">
+                <h2><?php echo $post->post_title; ?></h2>
+                <p><?php echo $post->post_excerpt; ?></p>
+                <!-- <pre>
+                <?php //print_r($post); ?>
+                </pre> -->
+                <!-- <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p> -->
+                <p><a class="btn btn-default" href="<?php echo $post->guid; ?>" role="button">Learn more &raquo;</a></p>
+            </div>
+            <?php endwhile; ?>
+            <?php else : ?>
+                <!-- no 'cards' found -->
+            <?php endif; ?>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
